278d31600bd5e2d5e079b42d2d5da043ba1a1bff
SaraUlan <220872@astanait.edu.kz>
2023-05-01 16:27:23 +0600

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
+import java.util.*;
+import java.io.*;
+
+import static java.lang.Math.pow;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            System.out.println(a+ pow(2,i)*b+pow(2,i++));
+        }
+        in.close();;
     }
 }
+
+
+
+
