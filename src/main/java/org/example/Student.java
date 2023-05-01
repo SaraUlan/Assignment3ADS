43a5f32ccbd06cd0161b43c19d2ac362865bd489
SaraUlan <220872@astanait.edu.kz>
2023-05-01 13:43:31 +0600

    student

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Student {
+    private String name;
+    private String lastname;
+
+    public Student(){}
+
+    public Student(String name, String lastname){
+        this.name = name;
+        this.lastname = lastname;
+    }
+}
