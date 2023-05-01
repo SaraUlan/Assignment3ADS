413d378127b807b155ec826ecf7eea9ae1a1ab20
SaraUlan <220872@astanait.edu.kz>
2023-05-01 14:04:53 +0600

    hash code

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -10,4 +10,18 @@ public class Student {
         this.name = name;
         this.lastname = lastname;
     }
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        String strKey = String.valueOf(this.name + " " + this.lastname);
+        for (int i = 0; i < strKey.length(); i++) {
+            hash += (int) strKey.charAt(i);
+        }
+        return hash;
+    }
+
+    @Override
+    public String toString(){
+        return "%s %s".formatted(this.name, this.lastname);
+    }
 }
